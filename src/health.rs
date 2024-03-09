30a34333803deba95610bf9c13cfb9cfb7136bfc
Uros Malesevic <Uros.Malesevic@bam.fan>
2024-03-09 13:03:10 +0100

    Add swagger

---
+++ src/health.rs
@@ -0,0 +1,39 @@
+use actix_web::{
+    get,
+    web::{self},
+    HttpResponse, Responder,
+};
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+#[derive(Serialize, Deserialize, ToSchema, Clone, Debug)]
+pub(super) struct GenericResponse {
+    #[schema(example = "success")]
+    pub status: String,
+    #[schema(example = "something")]
+    pub message: String
+}
+
+/// Do a health check.
+/// ```text
+/// curl localhost:8080/v1/health
+/// ```
+#[utoipa::path(get, path = "/health",
+    responses(
+        (status = 200, description = "Health check", body = GenericResponse)
+    )
+)]
+#[get("/health")]
+pub(super) async fn health_handler() -> impl Responder {
+    let json_response = &GenericResponse {
+        status: "success".to_string(),
+        message: "something".to_string(),
+    };
+    HttpResponse::Ok().json(json_response)
+}
+
+pub fn config(conf: &mut web::ServiceConfig) {
+    let scope = web::scope("").service(health_handler);
+
+    conf.service(scope);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
 use crate::user::dsl::*;
+use actix_web::{App, HttpServer};
 use diesel::prelude::*;
 use social_web_service::models::*;
 use social_web_service::schema::user::{self, username};
 use social_web_service::*;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::*;
 use uuid::Uuid;
 
-fn main() {
+mod health;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    #[derive(OpenApi)]
+    #[openapi(
+        paths(health::health_handler),
+        components(
+            schemas(health::GenericResponse)
+        ),
+        tags(
+            (name = "health", description = "Health check endpoints.")
+        ),
+    )]
+    struct ApiDoc;
+
+    HttpServer::new(move || {
+        App::new()
+            .service(
+                SwaggerUi::new("/swagger-ui/{_:.*}")
+                    .url("/api-docs/openapi.json", ApiDoc::openapi()),
+            )
+            .configure(health::config)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
+}
+
+fn db_test_crud() {
     let page_size = 100;
 
     let connection = &mut establish_connection();
